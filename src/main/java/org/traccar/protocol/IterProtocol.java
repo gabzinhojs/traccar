7bbdc26b451752d3933624d5d7a4ab0572a7a38e
gabriel <victorgaborges.svl@gmail.com>
2025-02-02 19:28:09 -0300

    Update IterProtocol.java

--- src/main/java/org/traccar/protocol/IterProtocol.java
+++ src/main/java/org/traccar/protocol/IterProtocol.java
@@ -1,3 +1,11 @@
+package org.traccar.protocol;
+
+import javax.inject.Inject;
+import org.traccar.BaseProtocol;
+import org.traccar.TrackerServer;
+import org.traccar.config.Config;
+import org.traccar.pipeline.PipelineBuilder;
+
 public class IterProtocol extends BaseProtocol {
     
     @Inject
